7e460080bee9547f90342dfc40eef4445d438243
SudattaHor <sudatta_hor@brown.edu>
2023-01-29 00:04:10 -0500

    funcs and models

--- team_solutions/team_ytterbium/func.py
+++ team_solutions/team_ytterbium/func.py
@@ -1,30 +1,31 @@
-import numpy as np
+from process_input import read_input
+import pickle as pk
 
-def predict(feature):
-	'''
-	input: 
-		feature (nd.array): feature
-	return :
-		p (int): prediction result, 1 for spam 0 for no
-	'''
-	s = feature.sum()
-	return 1 if s > 50 else 0
+
+def predict(feature, qflag=True):
+    '''
+    input:
+            feature (nd.array): feature
+    return :
+            p (int): prediction result, 1 for spam 0 for no
+    '''
+    if qflag:
+        filename = "models/qsvc.pkl"
+    else:
+        filename = "models/svc.pkl"
+    with open(filename, "rb") as fd:
+        model = pk.load(fd)
+    return model.predict(feature)
 
 
 def transform(text):
-	'''
-	input:
-		text (str): email body text
-	return:
-		output (nd.array): feature
-
-	'''
-	n = 5
-	num = len(text)
-	output = np.zeros(n)
-	for i in range(5):
-		output[i] = num + i 
-	return output
+    '''
+    input:
+            text (str): email body text
+    return:
+            output (nd.array): feature
+    '''
+    return read_input(text)
 
 
 if __name__ == '__main__':
--- team_solutions/team_ytterbium/process_data.py
+++ team_solutions/team_ytterbium/process_data.py
@@ -1,7 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn import decomposition
-from sklearn import datasets
 import csv
 import pickle as pk
 
