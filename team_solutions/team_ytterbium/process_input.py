cb097d5a89c0d01ec312d0026940d72a695de3c3
SudattaHor <sudatta_hor@brown.edu>
2023-01-28 22:47:28 -0500

    process input to spamebase features

---
+++ team_solutions/team_ytterbium/process_input.py
@@ -0,0 +1,82 @@
+import numpy as np
+import re
+
+word_list = [
+    "you",
+    "address",
+    "all",
+    "3d",
+    "our",
+    "over",
+    "remove",
+    "internet",
+    "order",
+    "mail",
+    "receive",
+    "will",
+    "people",
+    "report",
+    "addresses",
+    "free",
+    "business",
+    "email",
+    "you",
+    "credit",
+    "your",
+    "font",
+    "000",
+    "money",
+    "hp",
+    "hpl",
+    "george",
+    "650",
+    "lab",
+    "labs",
+    "telnet",
+    "857",
+    "data",
+    "415",
+    "85",
+    "technology",
+    "1999",
+    "parts",
+    "pm",
+    "direct",
+    "cs",
+    "meeting",
+    "original",
+    "project",
+    "re",
+    "edu",
+    "table",
+    "conference"
+]
+
+char_list = [
+    ";",
+    "(",
+    "[",
+    "!",
+    "$",
+    "#"
+]
+
+
+def read_input(input):
+    input_words = re.findall(r'[a-zA-Z0-9]+', input.lower())
+    print(input_words)
+    initial_features = np.zeros(57)
+    i = 0
+    for word in word_list:
+        initial_features[i] = input_words.count(word)
+        i += 1
+    for char in char_list:
+        initial_features[i] = input.count(char)
+        i += 1
+    seq_cap = np.array(list(map(len, re.findall(r"[A-Z]+", input))))
+    if len(seq_cap) == 0:
+        return initial_features
+    initial_features[i] = np.average(seq_cap)
+    initial_features[i + 1] = np.max(seq_cap)
+    initial_features[i + 2] = np.sum(seq_cap)
+    return initial_features
