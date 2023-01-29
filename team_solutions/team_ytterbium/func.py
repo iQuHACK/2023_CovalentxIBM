9e5e46f52fbdff8951a2d8ac48c2faf30d18a3ae
Weisewill <iamwtchiu@gmail.com>
2023-01-28 22:53:50 -0500

    Update

--- team_solutions/team_ytterbium/func.py
+++ team_solutions/team_ytterbium/func.py
@@ -1,7 +1,24 @@
 import numpy as np
 
+def predict(feature):
+	'''
+	input: 
+		feature (nd.array): feature
+	return :
+		p (int): prediction result, 1 for spam 0 for no
+	'''
+	s = feature.sum()
+	return 1 if s > 50 else 0
+
 
 def transform(text):
+	'''
+	input:
+		text (str): email body text
+	return:
+		output (nd.array): feature
+
+	'''
 	n = 5
 	num = len(text)
 	output = np.zeros(n)
